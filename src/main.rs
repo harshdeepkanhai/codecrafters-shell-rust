9452ceea6ef22ed505810c0e9e2aa43cebc81e4d
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2026-06-17 03:03:06 +0530

    uncomment code to read user input and display command not found message

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,8 @@ use std::io::{self, Write};
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    print!("{command}: command not found");
     io::stdout().flush().unwrap();
 }
